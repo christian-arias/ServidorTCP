e6013ff1806901131cc2d76b5fafff043174db55
Christian Arias <christian_arias_89@hotmail.com>
2014-10-02 19:12:39 -0500

    Clase vServidor
    Se agrega una ventana para ejecutar el Servidor y también un cliente
    para probar que la conexión sirve.

---
+++ ServidorTCP/src/servidortcp/Cliente.java
@@ -0,0 +1,89 @@
+package servidortcp;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Christian Arias
+ * @email christian_arias_89@homail.com
+ */
+public class Cliente implements Runnable { //Implementa la interfaz Runnable
+    public Socket s;
+    public ObjectInputStream ois;
+    public ObjectOutputStream oos;
+    
+    //Constructor de clase Cliente
+    public Cliente(){
+    }
+
+    public void run() {
+        try{
+            s = new Socket("localhost", 9999);
+//            ois = new ObjectInputStream(s.getInputStream());
+//            oos = new ObjectOutputStream(s.getOutputStream());
+            System.out.println("Conexion exitosa al servidor");
+            this.recibir();
+        } catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+    
+    //Cerrar todos los buffers y el socket para evitar que el puerto quede abierto
+    public void cerrarCliente(){
+        try{
+            oos.close();
+            ois.close();
+            s.close();
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+    
+//    public void recibir(){
+//        try{
+//            while(true){
+//                Object aux = ois.readObject();
+//                if (aux != null && aux instanceof String){
+//                    System.out.println((String) aux);
+//                }
+//            }
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//    }
+    
+    public void recibir(){
+//        try{
+//            while(true){
+//                Object aux = ois.readObject();
+//                ResultSet send = null;
+//                String h;
+//                if (aux != null){
+//                    System.out.println(aux);
+//                    this.consulta();
+//                }
+//            }
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+        try{
+            BufferedInputStream bis = new BufferedInputStream(this.s.getInputStream());
+            InputStreamReader isr = new InputStreamReader(bis,"US-ASCII");
+            StringBuffer instr = new StringBuffer();
+            int c;
+            while ( (c = isr.read()) != 37) instr.append( (char) c);
+
+            System.out.println(instr);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        
+    }
+}
